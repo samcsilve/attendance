82b9994c25e944eaa1e3137e54be1fb3435e28b3
samcsilve <samcsilve@gmail.com>
2020-11-28 10:03:31 -0600

    fixed config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false,
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
